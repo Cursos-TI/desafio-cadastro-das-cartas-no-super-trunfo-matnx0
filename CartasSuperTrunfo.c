2fb2ea623c8ebf997319952580052c4e2c4db6fe
Dyft <151576186+matnx0@users.noreply.github.com>
2025-08-26 23:45:09 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -25,56 +25,56 @@ int main() {
 
     // Solicita ao usuário que insira os dados para a primeira carta.
     printf("--- Cadastro da Primeira Carta ---\n");
-    printf("Digite a populacao: ");
+    printf("Digite a população: ");
 
     // "scanf" lê o valor digitado pelo usuário e armazena na variável.
     // O "%d" é um especificador de formato para inteiros.
     // O "&"" é o operador de endereço, necessário para o scanf.
     scanf("%d", &populacao_carta1);
 
-    printf("Digite a area: ");
+    printf("Digite a área: ");
     // O "%f" é um especificador de formato para float.
     scanf("%f", &area_carta1);
 
     printf("Digite o PIB: ");
     scanf("%f", &pib_carta1);
 
-    printf("Digite o numero de pontos turisticos: ");
+    printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos_carta1);
 
     // Solicita ao usuário que insira os dados para a segunda carta.
     printf("\n--- Cadastro da Segunda Carta ---\n");
-    printf("Digite a populacao: ");
+    printf("Digite a população: ");
     scanf("%d", &populacao_carta2);
 
-    printf("Digite a area: ");
+    printf("Digite a área: ");
     scanf("%f", &area_carta2);
 
     printf("Digite o PIB: ");
     scanf("%f", &pib_carta2);
 
-    printf("Digite o numero de pontos turisticos: ");
+    printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontos_turisticos_carta2);
 
     // Exibe os dados da primeira carta.
     printf("\n--- Dados da Primeira Carta ---\n");
 
     // O "printf" exibe a mensagem e o valor da variável.
-    printf("Populacao: %d\n", populacao_carta1);
+    printf("População: %d\n", populacao_carta1);
 
     // O "\n" insere uma quebra de linha para organizar a saída.
-    printf("Area: %.2f km2\n", area_carta1);
+    printf("Área: %.2f km2\n", area_carta1);
     
     // O "%.2f" formata o float para exibir apenas duas casas decimais.
-    printf("PIB: %.2f trilhoes\n", pib_carta1);
-    printf("Pontos Turisticos: %d\n", pontos_turisticos_carta1);
+    printf("PIB: %.2f trilhões\n", pib_carta1);
+    printf("Pontos Turísticos: %d\n", pontos_turisticos_carta1);
 
     // Exibe os dados da segunda carta.
     printf("\n--- Dados da Segunda Carta ---\n");
-    printf("Populacao: %d\n", populacao_carta2);
-    printf("Area: %.2f km2\n", area_carta2);
-    printf("PIB: %.2f trilhoes\n", pib_carta2);
-    printf("Pontos Turisticos: %d\n", pontos_turisticos_carta2);
+    printf("População: %d\n", populacao_carta2);
+    printf("Área: %.2f km2\n", area_carta2);
+    printf("PIB: %.2f trilhões\n", pib_carta2);
+    printf("Pontos Turísticos: %d\n", pontos_turisticos_carta2);
 
     // Retorna 0 para indicar que o programa foi executado com sucesso.
     return 0;
